16ef49f6583e756f5e405881ce4aab23223d2828
Mann Panchal <panchalmann541@gmail.com>
2024-07-18 02:33:41 +0530

    Model Trainer Done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,6 +11,9 @@ from dataclasses import dataclass # used to create class variables
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.data_trainer import ModelTrainerConfig
+from src.components.data_trainer import ModelTrainer
+
 # any input for data ingestion will be given through this class
 # if you just want to store variables then use this class, else for storing
 # functions and variables, use normal class
@@ -59,4 +62,7 @@ if __name__=="__main__":
 
     data_transformation = DataTransformation()
     train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
 
--- src/utils.py
+++ src/utils.py
@@ -34,12 +34,9 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             gs.fit(X_train,y_train)
 
             model.set_params(**gs.best_params_)
-            model.fit(X_train,y_train)
-
-            #model.fit(X_train, y_train)  # Train model
+            model.fit(X_train, y_train) # Train model
 
             y_train_pred = model.predict(X_train)
-
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
